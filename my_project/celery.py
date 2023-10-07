467c37154aa706229cebab855e7f7e1627b97d0e
Kaustubh <kaustubh@auberginesolutions.com>
2023-10-07 15:00:03 +0530

    Add celery beat job to run at certain time or certain period of time

--- my_project/celery.py
+++ my_project/celery.py
@@ -1,22 +1,33 @@
 from __future__ import absolute_import, unicode_literals
 import os
 
+from celery.schedules import crontab
 from celery import Celery
 from django.conf import settings
+from datetime import datetime
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
 
 app = Celery('my_project')
 app.conf.enable_utc = False
 
-app.conf.update(timezone = 'Asia/Kolkata')
+app.conf.update(timezone='Asia/Kolkata')
 
 app.config_from_object(settings, namespace='CELERY')
 
 # Celery Beat Settings
 app.conf.beat_schedule = {
-    
+    'Task_one_schedule' : {  # whatever the name you want
+        'task': 'user_app.tasks.test_func', # name of task with path
+        'schedule': crontab(hour=3, minute=18), # crontab() runs the tasks every minute
+    },
+    'Task_two_schedule' : {  # whatever the name you want
+        'task': 'user_app.tasks.test_date', # name of task with path
+        'schedule': 3000, # 30 runs this task every 3000 seconds
+        'args' : [datetime.now()] # arguments for the task
+    },
 }
+
 
 app.autodiscover_tasks()
 
--- user_app/tasks.py
+++ user_app/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+from datetime import datetime
 
 @shared_task(bind=True)
 def test_func(self):
@@ -6,3 +7,11 @@ def test_func(self):
     for i in range(10):
         print(i)
     return "Done"
+
+
+@shared_task(bind=True)
+def test_date(self, date_val):
+    # operations
+    print("Date got", date_val.strftime('%d-%m-%Y %H:%M:%S'))
+    print("Actual datetime", datetime.now().strftime('%d-%m-%Y %H:%M:%S'))
+    print("Operation eneded")
